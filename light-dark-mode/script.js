8d0288c9bd5ba1c205d2dc7edf585db00612cab0
Denys Tkachuk <ua.denist@gmail.com>
2020-08-14 22:36:02 +0300

    added light dark mode

---
+++ light-dark-mode/script.js
@@ -0,0 +1,46 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+const currentTheme = localStorage.getItem('theme');
+
+function imageMode(color) {
+    image1.src = `images/undraw_proud_coder_${color}.svg`;
+    image2.src = `images/undraw_feeling_proud_${color}.svg`;
+    image3.src = `images/undraw_conceptual_idea_${color}.svg`;
+}
+
+function toggleTheme(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+        : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode('dark') : imageMode('light');;
+}
+
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleTheme(true);
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleTheme(false);
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleTheme(true);
+    }
+}
